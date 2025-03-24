7e0c1cad0b7e8e1879893631c14afa494fc90233
LhGomesss <luishfeitosa01@gmail.com>
2025-03-24 08:08:45 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+
+Console.WriteLine ("FOGO!!!");
